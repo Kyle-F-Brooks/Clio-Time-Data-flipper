430a2ae777275cf3a4afa61d5e3b0ecb5d0f7458
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2024-04-23 17:19:59 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from os import listdir
+# from os.path import isfile, join
+
+myPath = "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Test Data"
+dirList = listdir(myPath)
+
+def createNewFileNames():
+    degrees = 5 # Set the degree seperation of each measurement
+
+
