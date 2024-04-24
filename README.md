046918bab0e42b1602bc330898ff72a827f8cd26
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2024-04-24 10:31:40 +0100

    Push Changes

---
+++ README.md
@@ -0,0 +1,7 @@
+![Void Acoustics](path)
+
+# Clio Time Data Flipper
+Author: Kyle Brooks
+
+## Description
+A script to mirror the Clio Time Data format files for use with Ease to create GSS(Generic Sound Source)
--- main.py
+++ main.py
@@ -5,6 +5,11 @@ myPath = "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Test Data
 dirList = listdir(myPath)
 
 def createNewFileNames():
-    degrees = 5 # Set the degree seperation of each measurement
+    degreesOfSeperation = 5 # Set the degree seperation of each measurement
+    newNames = []
+    currentDegree = 0
+    while currentDegree <= 360:
+        currentDegree += degreesOfSeperation
+
 
 
