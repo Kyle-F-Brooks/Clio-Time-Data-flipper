62b3c3c7d57da6121ddf4df5f93d838bb5597382
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2024-04-25 14:44:20 +0100

    upload test file

---
+++ testCode.py
@@ -0,0 +1,10 @@
+import os
+directory = 'Test Data'
+ 
+# iterate over files in
+# that directory
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(os.path.abspath(f))
