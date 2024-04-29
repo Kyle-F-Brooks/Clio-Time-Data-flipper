c0f098d3b88de92c8aef085b71371ef302546395
Kyle-F-Brooks <kylefrankbrooks1997@gmail.com>
2024-04-29 16:59:36 +0100

    script working

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import os
 import shutil
 
-def flippedNames(degreesOfSeperation):
+def normalNames(degreesOfSeperation):
     newNames = [] # empty array for new file names
-    currentMeridian = 0
-    m = 180
-    while currentMeridian < 360:
+    m = 0
+    while m <= 360:
         p = 0
         while  p <= 180:
             newNames.append("IR %s %s.txt" % ((m*100), (p*100)))
             p += degreesOfSeperation
-        
-        if m < 355:
-            m += degreesOfSeperation
-        elif m == 355:
-            m = 0
-
-        currentMeridian += degreesOfSeperation
+        m += degreesOfSeperation
 
     return newNames
 
 def invertedNames(degreesOfSeperation):
-    newNames = []
-    array1 = []
-    array2 = []
-    m = 0
+    # Creates an inverted list of original data. Parallels do not get inverted
+    newNames = [] # declare returned variable
+    array1 = []# first half
+    array2 = []# second half
+    m = 0 # set the meridian degree
     while m <= 180: #loop for first half
-        p = 180
+        p = 180 # set parallel degree
         while p >= 0: # count parallels backwars as they get inversed later
             array1.append("IR %s %s.txt" % ((m*100), (p*100)))
-            p -= degreesOfSeperation
-        m += degreesOfSeperation
+            p -= degreesOfSeperation # count down parallels by degrees of seperation
+        m += degreesOfSeperation # count up meridians by degrees of seperation
     if m >= 180: #loop for second half
         while m <= 360:
             p = 180
@@ -47,48 +41,25 @@ def invertedNames(degreesOfSeperation):
         newNames.append(x)
     return newNames
 
-def copy_and_rename(src_path, dest_path, new_name):
+def copyRename(src_path, dest_path, new_name):
     # Copy and rename the file
     new_path = f"{dest_path}/{new_name}"
     shutil.copy(src_path, new_path)
 
-def rename_files(inputPath, outputPath, originalNames, newNames):
+def processFiles(inputPath, outputPath, originalNames, newNames):
     # iterate over files in directory
-    for filename in os.listdir(inputPath):
-        # if numOfFiles < 180:
-        f = os.path.join(inputPath, filename)
-        # checking if it is a file
-        print(filename)
-        for i,j in enumerate(originalNames):
+    for i,j in enumerate(originalNames): # i = key, j = value
+        for filename in os.listdir(inputPath): 
+            f = os.path.join(inputPath, filename)
             if j==filename:
-                print(j)
                 if os.path.isfile(f):
-                    copy_and_rename(os.path.abspath(f), outputPath, newNames[i])
-            # print(os.path.abspath(f))
-        # elif numOfFiles == 180:
-        #     f = os.path.join(inputPath, filename)
-        #     # checking if it is a file
-        #     if os.path.isfile(f):
-        #         copy_and_rename(os.path.abspath(f), outputPath, newNames[numOfFiles])
-        #     numOfFiles += 1
-        # elif numOfFiles > 180:
-        #     f = os.path.join(inputPath, filename)
-        #     # checking if it is a file
-        #     if os.path.isfile(f):
-        #         copy_and_rename(os.path.abspath(f), outputPath, newNames[numOfFiles])
-        #     numOfFiles += 1
-
-inputPath = "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Test Data"
+                    copyRename(os.path.abspath(f), outputPath, newNames[i])
+# Set user variables
+inputPath = "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Input Data"
 outputPath = "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Output Data"
-originalOrder = os.listdir(inputPath)
-# print (len(dirList))
-# names = createNewFileNames()
-names = invertedNames(degreesOfSeperation=5)
-# file = open("test.txt", "w+")
-# file.write(str(names))
-# file.close()
-print(len(originalOrder))
-print(len(names))
+degrees = 5 # degrees between measurements
 
-# copy_and_rename("C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//test.txt", "C://Users//Kyle//Documents//Scripts//Clio Time Data flipper//Output Data", "jeff.txt")
-rename_files(inputPath, outputPath, originalOrder, names)
+correctOrder = normalNames(degrees) # create list of original order
+names = invertedNames(degrees) # create an inverted order
+
+processFiles(inputPath, outputPath, correctOrder, names) # copy and rename files
